7c11dbe949d73820992c2020bff3b30f857be02d
unknown <amitbishnoi.brw@gmail.com>
2019-10-30 15:23:51 +0530

    socket io is integrated with server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const socketIO = require('socket.io');
+
+const http = require('http');
 const path = require('path');
 
 const app = express();
+
+const server = http.createServer(app); // passing express as an argument
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,'/../public');
-console.log(publicPath);
 
+// connecting our socket to the server
+const io = socketIO(server);
+
+//middleware
 app.use(express.static(publicPath));
-app.listen(port,() => {
+
+
+io.on('connection',(socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect',function(){
+        console.log('User was disconnected');
+    });
+})
+server.listen(port,() => {
     console.log('server is running...');
 });
