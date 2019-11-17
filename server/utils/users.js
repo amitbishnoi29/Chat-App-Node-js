9329dad35b16f0ed1838d0a475672ada701495bf
unknown <amitbishnoi.brw@gmail.com>
2019-11-17 21:47:21 +0530

    class of users add , remove , getUsers

--- server/utils/users.js
+++ server/utils/users.js
@@ -7,9 +7,26 @@ class Users {
         this.users.push(user);
         return user;
     }
+    removeUser (id) {
+        this.users = this.users.filter( (user) => user.id !== id);
+    }
+
+    getUser (id) {
+        return this.users.filter((user) => user.id === id);
+    }
+
+    getUserList(room) {
+        let users = this.users.filter((user) => user.room === room);
+        return users.map((user) => user.name);
+    }
 }
 
 let user = new Users();
 user.addUser(1234,"amit",'fun');
-user.addUser(2345,'sunnu','fun');
+ user.addUser(2345,'sunnu','fun');
 console.log(user.users);
+// user.removeUser(1234);
+// console.log(user.users);
+console.log(user.getUserList('fun'));
+
+module.exports = {Users} ;
